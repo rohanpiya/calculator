c082c3c8b1d09883e5c1611553881700ebccb1eb
rohanpiya <piyar@dickinson.edu>
2026-06-28 16:35:03 +0545

    Add logic to update number variables when digits are clicked

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-function add(a,b) {
-    return a+b;
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(a,b) {
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(a,b) {
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(a,b) {
+function divide(a, b) {
     if (b === 0) {
         return "Sorry! Cannot divide by 0";
     }
-    return a/b;
+    return a / b;
 }
 
 function operate(firstNum, secondNum, operator) {
-    if (operator == '+'){
+    if (operator == '+') {
         return add(firstNum, secondNum);
     } else if (operator == '-') {
         return subtract(firstNum, secondNum);
@@ -28,3 +28,24 @@ function operate(firstNum, secondNum, operator) {
         return divide(firstNum, secondNum);
     }
 }
+
+let firstNum = '';
+let secondNum = '';
+let operator;
+
+const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('operator');
+const screen = document.querySelector('.screen');
+
+digits.forEach(digit => {
+    digit.addEventListener('click', () => {
+        if(operator == undefined) {
+            firstNum = firstNum + digit.textContent;
+        } else {
+            secondNum = secondNum + digit.textContent;
+        } 
+        const spanElement = document.createElement('span');
+        spanElement.textContent = digit.textContent;
+        screen.appendChild(spanElement);
+    })
+})
