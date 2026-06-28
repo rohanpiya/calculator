d01edc4814ae0bb190dcd3172765641a9eb71c16
rohanpiya <piyar@dickinson.edu>
2026-06-28 10:48:49 +0545

    Add and test the operate function

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ function divide(a,b) {
     return a/b;
 }
 
-let firstNum;
-let operator;
-let secondNum;
+function operate(firstNum, secondNum, operator) {
+    if (operator == '+'){
+        return add(firstNum, secondNum);
+    } else if (operator == '-') {
+        return subtract(firstNum, secondNum);
+    } else if (operator == '*') {
+        return multiply(firstNum, secondNum);
+    } else if (operator == '/') {
+        return divide(firstNum, secondNum);
+    }
+}
