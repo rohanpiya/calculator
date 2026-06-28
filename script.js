5849251c918848771218441122a8cb2cc3e2125c
rohanpiya <piyar@dickinson.edu>
2026-06-28 10:42:47 +0545

    Create and test functions for basic maths operations

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello, World!');
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
